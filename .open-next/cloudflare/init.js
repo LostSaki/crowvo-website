30ff394745f6dd52f32e027ed60e2586d29e6521
LostSaki <dylan.aruizmoya@gmail.com>
2026-04-26 13:36:17 -0500

    update

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1777217916285,
+    __BUILD_TIMESTAMP_MS__: 1777228460285,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
